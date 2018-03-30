92a3158a97faf0e1c93bc1b2c8759f23194fe34b
Jason Spiller <jason@thetargetlab.com>
2018-03-30 16:09:00 -0600

    localStorage is working with strings.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,73 @@
-// instantiate the form object
-const frmPost = document.getElementById('frmPost')
+$(function() {
 
-// add the event listener
-frmPost.addEventListener('submit', function(e){
-    e.preventDefault()
-
-		// create a new article
-		let newArticle = document.createElement('article');
-
+	// prepend todo to page
+	const addTodo  = (str) => {
 		// add the text to it
-		newArticle.innerHTML = document.getElementById('postText').value;
+		$('#todoList').prepend(
+			$('<li>')
+				.addClass('todo')
+				.html(str)
+		);
+	}
 
-		// add the new article to the DOM
-		document.getElementById('articleList').appendChild(newArticle);
+	const newTodo = (str) => {
+		// ensure there is text entered
+		if (str !== '') {
+
+			// add to the page
+			addTodo(str);
+
+			// add to localStorage, add delimiter if todos > 0
+			if (localStorage.getItem('todo') !== null) {
+				str = localStorage.getItem('todo') + '|todo|' + str;
+			}
+			localStorage.setItem('todo', str);
+
+			// reset error message
+			if ($('#msg').hasClass('error')) {
+				$('#msg')
+					.removeClass('error')
+					.text('What would you like Todo?');
+			}
+
+			// clear text from input
+			$('#todo').val('');
+
+		} else {
+			$('#msg')
+				.addClass('error')
+				.text('Try entering a Todo first');
+		}
+	};
+
+	// add the event listener
+	$('#frmTodo').on('submit', function(e){
+    e.preventDefault();
+		newTodo($('#todo').val());
+	});
+
+	const populateTodoList = () => {
+
+		// check to make sure there is a list
+		if (localStorage.getItem('todo') !== null) {
+
+			let arrTodos = localStorage.getItem('todo').split('|todo|');
+
+			for(let i=0; i < arrTodos.length; i++) {
+				addTodo(arrTodos[i])
+			}
+		}
+	}
+	populateTodoList();
+
+	let arrTest = ['1', '2', '3'];
+	console.log(arrTest);
+
+	localStorage.setItem('test', JSON.stringify(arrTest));
+
+	let arrTest = JSON.parse(localStorage.getItem('test'));
+	console.log(arrTest);
+
+
+
 });
